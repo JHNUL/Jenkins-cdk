8ec64f1744e680514f80861f1712a1f90eaf5241
Juhanir <juhani.riisio@gmail.com>
2023-02-03 20:12:41 +0200

    add install script

--- src/config.sh
+++ src/config.sh
@@ -1,6 +1,33 @@
-#!/bin/bash -xe
+#!/bin/bash
+set -x
+set -e
 
-# Update with optional user data that will run on instance start.
-# Learn more about user-data: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html
+# Update package registry
+yum update -y
 
-echo "hello instance"
+# Add Jenkins repo
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+# Import key-file from Jenkins-CI to enable installation
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+# Upgrade all packages
+yum upgrade
+
+# Install Java
+amazon-linux-extras install java-openjdk11 -y
+
+# Install Jenkins
+yum install jenkins -y
+
+# Enable Jenkins as daemon
+systemctl enable jenkins
+
+# Start Jenkins daemon
+systemctl start jenkins
+
+# Check Jenkins status
+systemctl status jenkins
+
+# Get the initial admin password
+cat /var/lib/jenkins/secrets/initialAdminPassword
