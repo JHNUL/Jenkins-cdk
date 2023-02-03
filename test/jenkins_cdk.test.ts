7025c8a0145a6d71abc8503be5542dcea63b0ab0
Juhanir <juhani.riisio@gmail.com>
2023-02-03 17:25:04 +0200

    Initial commit

---
+++ test/jenkins_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JenkinsCdk from '../lib/jenkins_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/jenkins_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JenkinsCdk.JenkinsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
